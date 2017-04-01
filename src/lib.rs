2119a718488ff3f5f9d76ab2c67602da11ca49b7
nabijaczleweli <nabijaczleweli@gmail.com>
2017-04-01 22:05:43 +0200

    Add top-level doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! The [OWAPI](https://github.com/SunDwarf/OWAPI) bindings.
+//!
+//! Currently only [v3](https://github.com/SunDwarf/OWAPI/blob/8167f61d965cdfd5e9604fcd3605f11fd4425585/api.md) is supported.
+//!
+//! Only the latest version is reexported at top-level.
+
+
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
